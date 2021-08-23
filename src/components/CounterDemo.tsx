8b6117ffe8e7482899f9ec229ac917e52a9f1659
Vasiliki Pasalidi-Chantzi <van3pasa@hotmail.com>
2021-08-23 18:06:03 +0300

    Add +5 +10 -5 -1 buttons and fix reset button

--- src/components/CounterDemo.tsx
+++ src/components/CounterDemo.tsx
@@ -8,15 +8,29 @@ function CounterDemo(): JSX.Element {
     queueRerenderWithNewCounterValue(counterValueFromCurrentRender + 1);
   };
 
-  const handleReset = () => {
-    // TODO reset counter to 0 here
-  }
+  const handleReset = () => queueRerenderWithNewCounterValue(0)
+
+  const handleAddFive = () => queueRerenderWithNewCounterValue(counterValueFromCurrentRender + 5)
+
+  const handleMinusFive = () => queueRerenderWithNewCounterValue(counterValueFromCurrentRender-5)
+  
+  const handleMinusOne =() => queueRerenderWithNewCounterValue(counterValueFromCurrentRender-1)
+
+  const handleAddTen =() => queueRerenderWithNewCounterValue(previousValue => previousValue + 10)
 
   return (
     <>
       <h1>Counter Demo</h1>
       <p>Current value: {counterValueFromCurrentRender}</p>
       <button onClick={handleAddOne}>+1</button>
+      <br />
+      <button onClick = {handleMinusOne}>-1</button>
+      <br />
+      <button onClick={handleAddFive}>+5</button>
+      <br />
+      <button onClick={handleMinusFive}>-5</button>
+      <br />
+      <button onClick={handleAddTen}>+10</button>
       <hr />
       <button onClick={handleReset}>Reset counter</button>
     </>
